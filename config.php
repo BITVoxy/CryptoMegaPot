9ac839e2d204ff8f23968908b1450b03e1b3eaad
Lance Cameron <vishtany@gmail.com>
2015-01-12 19:38:55 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = '';
+$dbname = '';
+$dbuser = '';
+$db = new mysqli(); // Edit later
+/* Below are BLOCK.io Configurations */
+$bIO_secret = ''
+// 
+$bIO_btckey = '';
+$bIO_dogekey = '';
+$bIO_ltckey = '';
+?>
